637ad5d31e580cd726f547bb6abc470a57334137
martin.haesler <martin.haesler@gmail.com>
2015-05-14 17:42:35 +0100

    Add fromJson method.

--- lib/task.dart
+++ lib/task.dart
@@ -5,8 +5,15 @@ class Task {
   DateTime scheduled;
   int timestamp;
   Set<String> taskcategories = new Set();
-  
-  Task(this.summary, [this.scheduled=null]) {
+
+  Task(this.summary, [this.scheduled = null]) {
     this.timestamp = new DateTime.now().millisecondsSinceEpoch;
+  }
+  Task.fromJson(json) {
+    Map data = json;
+    var sched = data['scheduled'];
+    summary = data['summary'];
+    timestamp = data['timestamp'];
+    scheduled = data['scheduled'];
   }
 }
