a7dc83e05d70e42b561ba3242f1154b888f6e4c9
martin.haesler <martin.haesler@gmail.com>
2015-05-15 20:10:38 +0100

    Add reflector in main()

--- web/main.dart
+++ web/main.dart
@@ -4,11 +4,14 @@
 
 
 import 'package:angular2/angular2.dart';
+import 'package:dangdays/new_task.dart';
 
 // These imports will go away soon:
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
 import 'package:angular2/src/reflection/reflection_capabilities.dart' show ReflectionCapabilities;
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  reflector.reflectionCapabilities = new ReflectionCapabilities();
+  bootstrap(NewTask);
+
 }
