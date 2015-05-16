2a16565ba00d306b995a880d3d637f6fd58a4d97
martin.haesler <martin.haesler@gmail.com>
2015-05-16 20:25:07 +0100

    Implement saveToStorage function.

--- lib/storageservice.dart
+++ lib/storageservice.dart
@@ -1,3 +1,13 @@
 library storageservice;
 
-import 'package:dangdays/task.dart';
+import 'package:dangdays/task.dart' show Task;
+import 'dart:convert';
+
+final String STORAGE_KEY = 'dangdays';
+
+List<Task> tasklist = [];
+
+void saveToStorage(Task task) {
+  tasklist.add(task);
+  window.localStorage[STORAGE_KEY] = JSON.encode(tasklist);
+}
