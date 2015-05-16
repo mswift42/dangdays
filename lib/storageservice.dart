f71709d12c8a0e4f6b748a5c8d55a259db833108
martin.haesler <martin.haesler@gmail.com>
2015-05-16 20:31:44 +0100

    Implement loadFromStorage function.

--- lib/storageservice.dart
+++ lib/storageservice.dart
@@ -1,13 +1,29 @@
 library storageservice;
 
-import 'package:dangdays/task.dart' show Task;
-import 'dart:convert';
+import 'package:dangdays/task.dart' show Task, fromJson;
+import 'dart:convert'
 
 final String STORAGE_KEY = 'dangdays';
 
 List<Task> tasklist = [];
 
+// add a task to tasklist. JSON encode and store said list
+// to localstorage.
 void saveToStorage(Task task) {
   tasklist.add(task);
   window.localStorage[STORAGE_KEY] = JSON.encode(tasklist);
+}
+
+// load the under key STORAGE_KEY stored tasklist.
+List<Task> loadFromStorage() {
+  if (window.localStorage.containsKey(STORAGE_KEY)) {
+    List<Task> tasks = [];
+    List stored = JSON.decode(window.localStorage[STORAGE_KEY]);
+    stored.forEach((i) {
+          Task t = new Task.fromJson(i);
+          tasks.add(t);
+        });
+    return tasks;
+  }
+  return [];
 }
