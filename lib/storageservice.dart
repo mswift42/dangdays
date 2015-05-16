b8e884364464473421a45599e7cf093893610bec
martin.haesler <martin.haesler@gmail.com>
2015-05-16 20:40:21 +0100

    Comment deleteTaskInStorage function.

--- lib/storageservice.dart
+++ lib/storageservice.dart
@@ -28,6 +28,8 @@ List<Task> loadFromStorage() {
   return [];
 }
 
+// delete the task in stored tasklist, which timestamp is identical
+// to the given task.
 void deleteTaskInStorage(Task task) {
   var sl = loadFromStorage().where((i) => i.timestamp != task.timestamp);
   window.localStorage[STORAGE_KEY] = JSON.encode(sl);
