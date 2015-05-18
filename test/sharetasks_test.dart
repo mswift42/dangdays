329065d522b2c96eb8f72049c1ad68263e194b64
martin.haesler <martin.haesler@gmail.com>
2015-05-18 20:39:28 +0100

    dartfmt testfile.

--- test/sharetasks_test.dart
+++ test/sharetasks_test.dart
@@ -4,9 +4,9 @@ import 'package:dangdays/task.dart' show Task;
 
 void main() {
   test('setTasks sets list tasks to the given tasklist', () {
-  Task t1 = new Task('task1');
-  Task t2 = new Task('task2');
-    setTasks([t1,t2]);
+    Task t1 = new Task('task1');
+    Task t2 = new Task('task2');
+    setTasks([t1, t2]);
     var tl = allTasks();
     expect(t1[0].summary, 'task1');
   });
