092f31f0c44301b32477aeedc6603bd0b6854e0c
martin.haesler <martin.haesler@gmail.com>
2015-05-18 20:38:40 +0100

    add setTasks  test.

--- test/sharetasks_test.dart
+++ test/sharetasks_test.dart
@@ -1,2 +1,13 @@
 import 'package:test/test.dart';
+import 'package:dangdays/sharetasks.dart';
+import 'package:dangdays/task.dart' show Task;
 
+void main() {
+  test('setTasks sets list tasks to the given tasklist', () {
+  Task t1 = new Task('task1');
+  Task t2 = new Task('task2');
+    setTasks([t1,t2]);
+    var tl = allTasks();
+    expect(t1[0].summary, 'task1');
+  });
+}
