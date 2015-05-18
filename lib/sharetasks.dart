90c13d7dbb428f28033da3a9b7dd33eb863621bf
martin.haesler <martin.haesler@gmail.com>
2015-05-18 15:58:16 +0100

    Add addTask, allTasks functions.

--- lib/sharetasks.dart
+++ lib/sharetasks.dart
@@ -1,3 +1,13 @@
 library sharetasks;
 
+import 'package:dangdays/task.dart' show Task;
+
 List<Task> tasks = [];
+
+void addTask(Task task) {
+  tasks.insert(0, task);
+}
+
+List<Task> allTasks() {
+  return tasks;
+}
