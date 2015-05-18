e9037e33b3960ce894c2d674e091f326a8a652f1
martin.haesler <martin.haesler@gmail.com>
2015-05-18 13:27:59 +0100

    Add library list_tasks

---
+++ lib/list_tasks.dart
@@ -0,0 +1,11 @@
+library list_tasks;
+
+import 'package:angular2/angular2.dart';
+import 'package:dangdays/task.dart' show Task;
+import 'package:dangdays/storageservice.dart' show loadFromStorage;
+
+@Component(selector: 'list-tasks')
+
+@View(
+  templateUrl: '../views/listtasks.html'
+)
