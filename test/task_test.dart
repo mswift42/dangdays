e6d2b9d824d9ac83e2805da9b8a8a260ae169dc6
martin.haesler <martin.haesler@gmail.com>
2015-05-19 13:19:44 +0100

    Check Task constructor.

--- test/task_test.dart
+++ test/task_test.dart
@@ -1,4 +1,16 @@
 import 'package:test/test.dart';
 import 'package:dangdays/task.dart';
 
-
+void main() {
+  test('Task instances are constructed correctly', () {
+    Task t1 = new Task('task1');
+    expect(t1.summary, 'task1');
+    expect(t1.scheduled, null);
+    expect((t1.timestamp > 0) , true);
+    DateTime d1 = new DateTime.now();
+    Task t2 = new Task('task2', d1);
+    expect(t2.summary, 'task2');
+    expect(t2.scheduled, isNotNull);
+    expect((t2.timestamp != t2.scheduled), true);
+  });
+}
