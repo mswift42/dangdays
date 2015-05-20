897c84807b669d4da142ce84c962e240e13417c0
martin.haesler <martin.haesler@gmail.com>
2015-05-20 22:18:37 +0100

    Import reflection libraries.

--- test/list_tasks_test.dart
+++ test/list_tasks_test.dart
@@ -1,5 +1,13 @@
 import 'package:test/test.dart';
 import 'dart:html';
 import 'package:dangdays/list_tasks.dart';
+import 'package:angular2/angular2.dart';
+
+import 'package:angular2/src/reflection/reflection.dart' show reflector;
+import 'package:angular2/src/reflection/reflection_capabilities.dart' show ReflectionCapabilities;
 
 
+void main() {
+  reflector.reflectionCapabilities = new ReflectionCapabilities();
+  bootstrap(ListTasks);
+}
