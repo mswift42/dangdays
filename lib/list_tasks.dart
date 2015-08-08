459f9fdebd42e0af7ef577c3bee420ca9e6fbcf5
mswift42 <martin.haesler@gmail.com>
2015-08-08 19:33:45 +0100

    use ng-for for const directives

--- lib/list_tasks.dart
+++ lib/list_tasks.dart
@@ -2,16 +2,16 @@ library list_tasks;
 
 import 'package:angular2/angular2.dart';
 import 'package:dangdays/task.dart' show Task;
-import 'package:dangdays/sharetasks.dart' show allTasks;
+import 'package:dangdays/sharetasks.dart' show tasks;
 
 @Component(selector: 'list-tasks')
 
 @View(
-  templateUrl: '../views/listtasks.html',
-  directives: const[For]
+  templateUrl: '../web/views/listtasks.html',
+  directives: const[NgFor]
 )
 
 
 class ListTasks {
-  List<Task> tasks = allTasks();
+  List<Task> tasklist = tasks;
 }
